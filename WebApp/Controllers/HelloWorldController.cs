9a682ea1aa5cb738edc08bc91fea7ce4515f008d
Stephen <stewxy13@gmail.com>
2023-09-16 04:37:51 +1200

    Add project files.

---
+++ WebApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+
+namespace WebApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+/*        public IActionResult Index()
+        {
+            return View();
+        }*/
+
+        public string Index()
+        {
+            return ("Hello");
+        }
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, num is {ID}");
+        }
+
+    }
+}
